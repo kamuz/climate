c273d15b1e0017b7939c1fb9b3cd20e2a850ec91
Vladimir Kamuz <v.kamuz@gmail.com>
2019-10-08 17:25:41 +0300

    Mobile top menu

--- js/src/main.js
+++ js/src/main.js
@@ -1,56 +1,54 @@
-(function($) {
-    $(function() {
-
-        /**
-         * Top Menu
-         */
-        $('nav ul li a:not(:only-child)').click(function(e) {
-            $(this).siblings('.nav-dropdown').toggle();
-            e.stopPropagation();
-        });
-        $(document).mouseup(function(e){
-            var container = $(".dropdown");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
-                $('.nav-dropdown').hide();
-            }
-        });
-        $('#nav-toggle').on('click', function() {
-            this.classList.toggle('active');
-        });
-        $('.nav-dropdown .notifications a, .nav-dropdown .switch-account a').click(function() {
-            $(this).next('div').fadeIn();
-        });
-        $('.nav-dropdown .close').click(function() {
-            console.log("close");
-            $(this).parent().fadeOut();
-        });
-
-        /**
-         * Next Slide inside modal
-         */
-        $('.next-slide').click(function(e) {
-            e.preventDefault();
-            $(".carousel").carousel("next");
-        });
-
-        /**
-         * Init mask for phone, credit card etc.
-         */
-        $(":input").inputmask();
-        var clipboard = new ClipboardJS('.clipboard');
-
-        /**
-         * Copy to clipboard init
-         */
-        clipboard.on('success', function(event) {
-            console.log(event);
-            event.trigger.classList.add('copied');
-            window.setTimeout(function() {
-                event.trigger.classList.remove('copied');
-            }, 2000);
-        });
+$(document).ready(function() {
+    /**
+     * Top Menu
+     */
+    $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        e.stopPropagation();
     });
-})(jQuery);
+    $(document).mouseup(function(e) {
+        var container = $(".dropdown");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $('.nav-dropdown').hide();
+        }
+    });
+    $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+        $('.navigation nav .col-lg-6 > ul').toggle();
+        $('.navigation nav').toggleClass('show');
+    });
+    $('.nav-dropdown .notifications a, .nav-dropdown .switch-account a').click(function() {
+        $(this).next('div').fadeIn();
+    });
+    $('.nav-dropdown .close').click(function() {
+        $(this).parent().fadeOut();
+    });
+
+    /**
+     * Next Slide inside modal
+     */
+    $('.next-slide').click(function(e) {
+        e.preventDefault();
+        $(".carousel").carousel("next");
+    });
+
+    /**
+     * Init mask for phone, credit card etc.
+     */
+    $(":input").inputmask();
+    var clipboard = new ClipboardJS('.clipboard');
+
+    /**
+     * Copy to clipboard init
+     */
+    clipboard.on('success', function(event) {
+        console.log(event);
+        event.trigger.classList.add('copied');
+        window.setTimeout(function() {
+            event.trigger.classList.remove('copied');
+        }, 2000);
+    });
+});
 
 /**
  * Init Bootstrap validation
